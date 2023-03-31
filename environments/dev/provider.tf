0d7fcff61effa62c8729b86533c73cf3fd05a7ec
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-03-31 09:13:08 +0300

    Test 6

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -13,5 +13,5 @@ terraform {
 provider "google" {
   project                     = var.project_id
   region                      = var.region
-  impersonate_service_account = var.tf_service_account
+  #impersonate_service_account = var.tf_service_account
 }
