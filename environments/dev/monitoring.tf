a45cbedebebd9df7bfe1d4e4a7247b53dc54e4f7
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-08-31 08:19:28 +0300

    Add all microservices to monitoring

--- environments/dev/monitoring.tf
+++ environments/dev/monitoring.tf
@@ -1,3 +1,13 @@
+module "anonymizer" {
+  source          = "../../modules/monitor-service"
+  service_id      = "anonymizer"
+  controller_name = "anonymizer"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
 module "gtfsrt_vp_poller_kuopio" {
   source          = "../../modules/monitor-service"
   service_id      = "gtfsrt-vp-poller-kuopio"
@@ -12,6 +22,56 @@ module "gtfsrt_vp_poller_jyvaskyla" {
   source          = "../../modules/monitor-service"
   service_id      = "gtfsrt-vp-poller-jyvaskyla"
   controller_name = "gtfsrt-vp-poller-fi-jyvaskyla"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
+module "journey_matcher" {
+  source          = "../../modules/monitor-service"
+  service_id      = "journey-matcher"
+  controller_name = "journey-matcher"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
+module "mqtt_deduplicator" {
+  source          = "../../modules/monitor-service"
+  service_id      = "mqtt-deduplicator"
+  controller_name = "mqtt-deduplicator"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
+module "mqtt_pulsar_forwarder" {
+  source          = "../../modules/monitor-service"
+  service_id      = "mqtt-pulsar-forwarder"
+  controller_name = "mqtt-pulsar-forwarder"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
+module "pulsar_mqtt_forwarder_raportointi" {
+  source          = "../../modules/monitor-service"
+  service_id      = "pulsar-mqtt-forwarder-raportointi"
+  controller_name = "pulsar-mqtt-forwarder-raportointi"
+  cluster_name    = "prototype"
+  environment     = var.environment
+  region          = var.region
+  project_id      = var.project_id
+}
+
+module "pulsar_mqtt_forwarder_rtpi" {
+  source          = "../../modules/monitor-service"
+  service_id      = "pulsar-mqtt-forwarder-rtpi"
+  controller_name = "pulsar-mqtt-forwarder-rtpi"
   cluster_name    = "prototype"
   environment     = var.environment
   region          = var.region
