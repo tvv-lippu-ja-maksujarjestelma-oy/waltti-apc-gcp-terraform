6771824e99337e29914e2503311fe34cc46f61ee
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-09-13 14:10:19 +0300

    Bump QA terraform provider

--- environments/staging/provider.tf
+++ environments/staging/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.75.0"
+      version = "~> 4.82.0"
 
     }
   }
 
-  required_version = "~> 1.3"
+  required_version = "~> 1.5"
 }
 
 provider "google" {
