9b216605401c9890dd2c30c6603794f6cfe608b6
Hannes Heikkinen <hannes@vertics.co>
2025-08-14 15:08:25 +0300

    Separated management ui and mqtt traffic to own domains. Added TCP load balancer for mqtt.

--- modules/rabbitmq/main.tf
+++ modules/rabbitmq/main.tf
@@ -1,3 +1,7 @@
 resource "google_compute_global_address" "rabbitmq" {
   name = "rabbitmq"
 }
+
+resource "google_compute_global_address" "rabbitmq-ui" {
+  name = "rabbitmq-ui"
+}
--- modules/rabbitmq/outputs.tf
+++ modules/rabbitmq/outputs.tf
@@ -1,3 +1,7 @@
 output "rabbitmq_global_ip" {
   value = google_compute_global_address.rabbitmq.address
 }
+
+output "rabbitmq_ui_global_ip" {
+  value = google_compute_global_address.rabbitmq-ui.address
+}
