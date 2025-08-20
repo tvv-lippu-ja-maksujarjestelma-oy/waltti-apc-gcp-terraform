d06a5c2cf4c31bc2cfbef6d2c1264e5fe1e3875f
Petteri Zitting <petteri.zitting@solita.fi>
2025-08-20 15:13:31 +0300

    Enable DNS endpoint in dev cluster

--- environments/dev/cluster.tf
+++ environments/dev/cluster.tf
@@ -1,8 +1,9 @@
 module "cluster" {
-  source           = "../../modules/kubernetes"
-  environment      = var.environment
-  network_name     = module.network.network_name
-  management_cidrs = var.management_cidrs
-  project_id       = var.project_id
-  k8s_version      = "1.28.3-gke.1203001"
+  source               = "../../modules/kubernetes"
+  environment          = var.environment
+  network_name         = module.network.network_name
+  management_cidrs     = var.management_cidrs
+  project_id           = var.project_id
+  k8s_version          = "1.28.3-gke.1203001"
+  dns_endpoint_enabled = true
 }
