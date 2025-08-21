47d19261d95a9b75e151fb3e87027e0b26c8fc5f
Hannes Heikkinen <hannes@vertics.co>
2025-08-21 14:39:49 +0300

    Enabled cross-project DNS management for RabbitMQ

--- environments/dev/rabbitmq.tf
+++ environments/dev/rabbitmq.tf
@@ -1,4 +1,6 @@
 module "rabbitmq" {
-  source = "../../modules/rabbitmq"
-  region = var.region
+  source                 = "../../modules/rabbitmq"
+  region                 = var.region
+  project_id             = var.project_id
+  dns_service_account_id = module.cert-manager.dns_service_account_id
 }
--- modules/rabbitmq/main.tf
+++ modules/rabbitmq/main.tf
@@ -6,3 +6,9 @@ resource "google_compute_address" "rabbitmq" {
 resource "google_compute_global_address" "rabbitmq-ui" {
   name = "rabbitmq-ui"
 }
+
+resource "google_service_account_iam_member" "cert_manager_dns_workload" {
+  service_account_id = var.dns_service_account_id
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[cert-manager/cert-manager]"
+}
