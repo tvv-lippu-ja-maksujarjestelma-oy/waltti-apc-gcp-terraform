c6a4e0992ecd95a55118b847a66bd8e12b39e8f6
Hannes Heikkinen <hannes@vertics.co>
2025-10-30 15:05:05 +0200

    Added dns-based auth for staging cluster

--- environments/staging/cluster.tf
+++ environments/staging/cluster.tf
@@ -1,9 +1,10 @@
 module "cluster" {
-  source           = "../../modules/kubernetes"
-  environment      = var.environment
-  network_name     = module.network.network_name
-  management_cidrs = var.management_cidrs
-  project_id       = var.project_id
-  cluster_name     = var.environment
-  k8s_version      = "1.28.3-gke.1203001"
+  source               = "../../modules/kubernetes"
+  environment          = var.environment
+  network_name         = module.network.network_name
+  management_cidrs     = var.management_cidrs
+  project_id           = var.project_id
+  cluster_name         = var.environment
+  k8s_version          = "1.28.3-gke.1203001"
+  dns_endpoint_enabled = true
 }
