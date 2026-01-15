cb7b862321f4a584b769b7e83ade417ecc064fb8
Ilkka Lappeteläinen <ilkka.lappetelainen@gmail.com>
2026-01-15 13:22:04 +0200

    Update Terraform configurations for production environment
    - Updated Kubernetes version to 1.31.13-gke.1123000 in cluster.tf
    - Enabled DNS endpoint in cluster module
    - Changed KMS key ring location to europe-west3 in main.tf
    - Added attribute condition for GitHub OIDC provider in main.tf
    - Introduced new RabbitMQ module in rabbitmq.tf
    - Added dns_service_account_id variable in variables.tf
    - Updated Terraform lock file with new provider hashes

---
+++ environments/prod/rabbitmq.tf
@@ -0,0 +1,7 @@
+module "rabbitmq" {
+  source                 = "../../modules/rabbitmq"
+  region                 = var.region
+  project_id             = var.project_id
+  dns_service_account_id = var.dns_service_account_id
+}
+
